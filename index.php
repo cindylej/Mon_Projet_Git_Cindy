9b44f8c45f0c1d7678eaf2c788df436ce4e73a9a
Cindy LEJEUNE <cindylej@gmail.com>
2022-02-09 11:39:43 +0100

    dev carroussel + main index +footer

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=devicegit-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -45,11 +45,62 @@
       </div>
     </div>
   </nav>
+
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/images.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/téléchargement (2).jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/téléchargement (1).jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
     </header>
+
+    <main>
 
     <h1 class="display-4 text-center my-5">Mon Projet Git</h1>
 
-   
+<div class="container">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus laboriosam explicabo officia. Est in magni pariatur maxime amet explicabo rem, provident, alias aspernatur mollitia adipisci, quo quisquam earum ducimus aut aperiam repellendus inventore ut. Cumque ipsam omnis veniam asperiores, magnam itaque doloremque ipsa est! Voluptatibus repudiandae sint a minus in tempore ducimus ab accusantium ex sunt doloremque cupiditate, impedit perferendis cumque nobis voluptas consequuntur vel iste. Quaerat repudiandae est natus iure fuga alias, magnam odio nihil tempora cumque sunt aut illo fugiat reiciendis sapiente voluptatum excepturi ullam similique corrupti, officiis doloribus facere totam. Quibusdam, nisi! Tempore amet repellat eveniet reprehenderit cumque dolor quos earum, consectetur laudantium nisi eligendi fuga recusandae suscipit id accusantium odio ab magni? Praesentium voluptas debitis quas laborum deserunt necessitatibus iste nesciunt commodi. Blanditiis, quibusdam quia? Magnam rem, enim ducimus veniam ad temporibus nobis architecto. Impedit quas temporibus ducimus pariatur eaque eius vel obcaecati quae necessitatibus. Minus, ut labore voluptas mollitia dolore voluptatibus modi aspernatur explicabo fuga saepe, dolorum, dignissimos distinctio excepturi cumque veritatis molestiae nesciunt a repudiandae error. Vel et distinctio fugiat atque dolorum, inventore quidem, nulla ullam ratione error corporis ab sit unde vitae asperiores quam animi incidunt consequatur consequuntur adipisci mollitia, in labore quisquam.
+
+ </p> 
+
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus laboriosam explicabo officia. Est in magni pariatur maxime amet explicabo rem, provident, alias aspernatur mollitia adipisci, quo quisquam earum ducimus aut aperiam repellendus inventore ut. Cumque ipsam omnis veniam asperiores, magnam itaque doloremque ipsa est! Voluptatibus repudiandae sint a minus in tempore ducimus ab accusantium ex sunt doloremque cupiditate, impedit perferendis cumque nobis voluptas consequuntur vel iste. Quaerat repudiandae est natus iure fuga alias, magnam odio nihil tempora cumque sunt aut illo fugiat reiciendis sapiente voluptatum excepturi ullam similique corrupti, officiis doloribus facere totam. Quibusdam, nisi! Tempore amet repellat eveniet reprehenderit cumque dolor quos earum, consectetur laudantium nisi eligendi fuga recusandae suscipit id accusantium odio ab magni? Praesentium voluptas debitis quas laborum deserunt necessitatibus iste nesciunt commodi. Blanditiis, quibusdam quia? Magnam rem, enim ducimus veniam ad temporibus nobis architecto. Impedit quas temporibus ducimus pariatur eaque eius vel obcaecati quae necessitatibus. Minus, ut labore voluptas mollitia dolore voluptatibus modi aspernatur explicabo fuga saepe, dolorum, dignissimos distinctio excepturi cumque veritatis molestiae nesciunt a repudiandae error. Vel et distinctio fugiat atque dolorum, inventore quidem, nulla ullam ratione error corporis ab sit unde vitae asperiores quam animi incidunt consequatur consequuntur adipisci mollitia, in labore quisquam.
+
+</p> 
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus laboriosam explicabo officia. Est in magni pariatur maxime amet explicabo rem, provident, alias aspernatur mollitia adipisci, quo quisquam earum ducimus aut aperiam repellendus inventore ut. Cumque ipsam omnis veniam asperiores, magnam itaque doloremque ipsa est! Voluptatibus repudiandae sint a minus in tempore ducimus ab accusantium ex sunt doloremque cupiditate, impedit perferendis cumque nobis voluptas consequuntur vel iste. Quaerat repudiandae est natus iure fuga alias, magnam odio nihil tempora cumque sunt aut illo fugiat reiciendis sapiente voluptatum excepturi ullam similique corrupti, officiis doloribus facere totam. Quibusdam, nisi! Tempore amet repellat eveniet reprehenderit cumque dolor quos earum, consectetur laudantium nisi eligendi fuga recusandae suscipit id accusantium odio ab magni? Praesentium voluptas debitis quas laborum deserunt necessitatibus iste nesciunt commodi. Blanditiis, quibusdam quia? Magnam rem, enim ducimus veniam ad temporibus nobis architecto. Impedit quas temporibus ducimus pariatur eaque eius vel obcaecati quae necessitatibus. Minus, ut labore voluptas mollitia dolore voluptatibus modi aspernatur explicabo fuga saepe, dolorum, dignissimos distinctio excepturi cumque veritatis molestiae nesciunt a repudiandae error. Vel et distinctio fugiat atque dolorum, inventore quidem, nulla ullam ratione error corporis ab sit unde vitae asperiores quam animi incidunt consequatur consequuntur adipisci mollitia, in labore quisquam.
+
+</p> 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus laboriosam explicabo officia. Est in magni pariatur maxime amet explicabo rem, provident, alias aspernatur mollitia adipisci, quo quisquam earum ducimus aut aperiam repellendus inventore ut. Cumque ipsam omnis veniam asperiores, magnam itaque doloremque ipsa est! Voluptatibus repudiandae sint a minus in tempore ducimus ab accusantium ex sunt doloremque cupiditate, impedit perferendis cumque nobis voluptas consequuntur vel iste. Quaerat repudiandae est natus iure fuga alias, magnam odio nihil tempora cumque sunt aut illo fugiat reiciendis sapiente voluptatum excepturi ullam similique corrupti, officiis doloribus facere totam. Quibusdam, nisi! Tempore amet repellat eveniet reprehenderit cumque dolor quos earum, consectetur laudantium nisi eligendi fuga recusandae suscipit id accusantium odio ab magni? Praesentium voluptas debitis quas laborum deserunt necessitatibus iste nesciunt commodi. Blanditiis, quibusdam quia? Magnam rem, enim ducimus veniam ad temporibus nobis architecto. Impedit quas temporibus ducimus pariatur eaque eius vel obcaecati quae necessitatibus. Minus, ut labore voluptas mollitia dolore voluptatibus modi aspernatur explicabo fuga saepe, dolorum, dignissimos distinctio excepturi cumque veritatis molestiae nesciunt a repudiandae error. Vel et distinctio fugiat atque dolorum, inventore quidem, nulla ullam ratione error corporis ab sit unde vitae asperiores quam animi incidunt consequatur consequuntur adipisci mollitia, in labore quisquam.
+
+</p> 
+
+
+   </div>
+
+   </main>
+
+   <footer class="bg-dark text-center text-white py-3">
+       &copy; 2002 - CL - Mon Projet Git - Tous droit résevés
+
+   </footer>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
